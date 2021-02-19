62ab54bddf3db34a8527bfa1a44d8f4ba780dd38
Subham Mitra <subham.mitra21@gmail.com>
2021-02-19 23:34:49 +0530

    language translator added

---
+++ languagetranslate.js
@@ -0,0 +1,25 @@
+let input=document.querySelector('#input');
+let output=document.querySelector('#output');
+let translateButton=document.querySelector('#translate-button');
+const resetButton=document.querySelector('#reset-button');
+
+resetButton.addEventListener('click',()=>{
+  input.value="";
+  output.value="";
+  });
+
+translateButton.addEventListener('click',()=>{
+  const url = new URL('https://api.mymemory.translated.net/get');
+  const param = { q: input.value, langpair: 'en|Ben' };
+  Object.keys(param).forEach((key) => {
+      url.searchParams.append(key, param[key]);
+  });
+  url.search = new URLSearchParams(param).toString();
+  fetch(url)
+  .then((response) => response.json())
+      .then((json) => {
+          output.value = json.responseData.translatedText;
+      });
+});
+
+  
