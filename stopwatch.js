59ba5c4a3c4e918beae134d9fa5c1e836305f34a
Subham Mitra <subham.mitra21@gmail.com>
2021-02-28 23:39:53 +0530

    added milliseconds

--- stopwatch.js
+++ stopwatch.js
@@ -3,6 +3,7 @@ const resetButton=document.querySelector('#reset-button');
 const stopwatchHour=document.querySelector('#stopwatch-hh');
 const stopwatchMinute=document.querySelector('#stopwatch-mm');
 const stopwatchSecond=document.querySelector('#stopwatch-ss');
+const stopwatchMilliSecond=document.querySelector('#stopwatch-ms');
 
 
 let stopwatchId;let time=0;
@@ -16,6 +17,9 @@ stopwatchMinute.value='0'+ stopwatchMinute.value;
 if(stopwatchSecond.value.length===1)
     stopwatchSecond.value='0'+ stopwatchSecond.value;
 
+if(stopwatchMilliSecond.value.length===1)
+    stopwatchMilliSecond.value='00'+ stopwatchMilliSecond.value;    
+
 startButton.addEventListener('click',()=>{
 
   if(startButton.value==='Start'){
@@ -23,16 +27,19 @@ startButton.addEventListener('click',()=>{
   stopwatchId=setInterval(()=>{
    
    time++;
-   
-   stopwatchSecond.value=(time)%60;
-   stopwatchMinute.value=parseInt(time/60)%60;
-   stopwatchHour.value=parseInt(time/3600);
+   stopwatchMilliSecond.value=parseInt(time) % 1000;
+   stopwatchSecond.value=parseInt(time/1000)%60;
+   stopwatchMinute.value=parseInt((time/1000)/60)%60;
+   stopwatchHour.value=parseInt((time/1000)/60)/3600 % 60 ;
 
    /*
    stopwatchSecond.value=(time)%60;
    stopwatchMinute.value=(time/60)%60;
    stopwatchHour.value=(time/3600);
    */
+
+  if(stopwatchMilliSecond.value.length===1)
+  stopwatchMilliSecond.value='00'+ stopwatchMilliSecond.value;
    
    if(stopwatchSecond.value.length===1)
     stopwatchSecond.value='0'+ stopwatchSecond.value;
@@ -41,9 +48,9 @@ startButton.addEventListener('click',()=>{
     stopwatchMinute.value='0'+ stopwatchMinute.value;
   
     if(stopwatchHour.value.length===1)
-stopwatchHour.value='0'+ stopwatchHour.value;
+     stopwatchHour.value='0'+ stopwatchHour.value;
 
-  },1000);
+  },1);
 
 }else{
   clearInterval(stopwatchId);
@@ -59,9 +66,12 @@ stopwatchHour.value='0'+ stopwatchHour.value;
 
 resetButton.addEventListener('click',()=>{
   
+  clearInterval(stopwatchId);
   startButton.value='Start';
   stopwatchHour.value='0'+0;
   stopwatchMinute.value='0'+0;
   stopwatchSecond.value='0'+0;
-  clearInterval(stopwatchId);
+  stopwatchMilliSecond.value='00'+0;
+  time=0;
+  
 });
